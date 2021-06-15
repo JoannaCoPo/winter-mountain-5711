a5dedfa41fdf394851509e8313fdb3ec1730ed06
JoannaCoPo <jococopo@gmail.com>
2021-06-15 15:48:29 -0600

    Update hpospitals controller

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -1,7 +1,7 @@
 class HospitalsController < ApplicationController
   def show
     @hospital = Hospital.find(params[:id])
-    @count = @hospital.doctors.doctor_count
+    @count = @hospital.doctor_count
     @doctor_universities = @hospital.doctors.doctor_universities
   end
 end
