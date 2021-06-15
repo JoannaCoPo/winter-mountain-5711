b5b280a23eddb1db0f7fb7816224dc2fd1fcfc78
JoannaCoPo <jococopo@gmail.com>
2021-06-15 15:44:58 -0600

    Use distinct instead of uniq for doctor_universities

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,13 +2,21 @@ class Doctor < ApplicationRecord
   belongs_to :hospital
   has_many :patient_doctors
   has_many :patients, through: :patient_doctors
-  #
-  def self.doctor_count
-    all.count
-  end
+
+  # new method in Hospital
+  # def self.doctor_count
+  #   all.count
+  # end
 
   def self.doctor_universities
-    collection = pluck(:university).uniq
+    # collection = pluck(:university).uniq           # original version
+    collection = distinct.pluck(:university)         # new version
+    # collection = pluck('DISTINCT university')      # also an option with SQL
     collection.join(', ')
   end
+
+  # def self.doctor_universities                    # option using select
+  #   collection = select('distinct university').map(&:university)
+  #   collection.join(', ')
+  # end
 end
